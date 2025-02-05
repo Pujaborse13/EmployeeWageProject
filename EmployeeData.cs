edf72ab7b0c6808bc51645b6cd8ee6ac5295058f
Puja Borse <pujaborse13@gmail.com>
2025-02-05 14:53:30 +0530

    Calculate Daily Employee Wage

--- EmployeeData.cs
+++ EmployeeData.cs
@@ -7,29 +7,44 @@ using System.Threading.Tasks;
 namespace EmployeeWage
 {
 
-    internal class EmployeeWage1
+    internal class EmployeeData
     {
+        const int WAGE_PER_HOUR = 20;
+        const int FULL_DAY_HOUR = 8;
+        
         //UC1: Employee Attendance
-
-        public void EmployeeAttendance()
+        public bool EmployeeAttendance()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 2);
 
-            if (empCheck == 1)
+            return empCheck == 1;
+
+        }
+
+
+
+        //UC2: Daily Employee Wage
+        public void DailyEmployeeWage()
+        {
+            if (EmployeeAttendance())
             {
-
-                Console.WriteLine("Employee is present");
-
+                Console.WriteLine("Employee is Present");
+                int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                Console.WriteLine("Daily Wage: " + dailyWage);
             }
 
-            else
-            {
-                Console.WriteLine("Employee is absent");
-
+            else {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Daily Wage: 0");
             }
+
         }
 
 
     }
+
+
+
+ 
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ class Program
     static void Main(string[] args)
     {
         EmployeeData employeeData = new EmployeeData();
-        employeeData.EmployeeAttendance();
+        employeeData.DailyEmployeeWage();
+
 
     }
 
